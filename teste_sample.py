d2ed751ee083dff7e20b278f8edfd204d7e1ce80
“arthurgerhardt” <renewedjesus@gmail.com>
2025-05-27 16:54:25 -0300

    Adding some files and updating README.md .

---
+++ teste_sample.py
@@ -0,0 +1,10 @@
+# contente of teste_sample.py
+import unittest
+from sample import soma, subtracao, multiplicacao, divisao
+
+def inc(x):
+    return x + 1
+
+def test_answer():
+    assert inc(3) == 5
+
